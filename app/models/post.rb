ba19eb5d55c7a5d706c57839ef19f56b9bf45ef8
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-24 22:13:07 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,8 @@ class Post < ActiveRecord::Base
   validate :is_title_case
 
   # New Code!!
-    before_validation :make_title_case
-
-  private
+  before_validation :make_title_case
+  # example: before_save :email_author_about_post  private
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
